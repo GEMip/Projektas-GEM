9cc5019778e32bc104e7df625da2838fce1d727d
Gedas <gedas.abc@gmail.com>
2022-02-08 13:11:22 +0000

    duper

---
+++ main.js
@@ -0,0 +1,13 @@
+// Logo padidinimas:
+
+// Teksto spalvos keitimas
+document.getElementsByClassName('nav-link').addEventListener('mouseover', mouseOver);
+document.getElementsByClassName('nav-link').addEventListener('mouseout', mouseOut);
+
+function mouseOver() {
+//	document.getElementsByClassName('nav-link').style.color = 'red';
+}
+
+function mouseOut() {
+//	document.getElementsByClassName('nav-link').style.color = 'black';
+}
