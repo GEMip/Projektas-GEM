152e7066d040445bc1587fc99d6562fca049be24
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-02-28 20:21:00 +0200

    vienodi nav tarpai

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Logo padidinimas:
 
 // Teksto spalvos keitimas
-document.getElementsByClassName('nav-link').addEventListener('mouseover', mouseOver);
-document.getElementsByClassName('nav-link').addEventListener('mouseout', mouseOut);
+// document.getElementsByClassName('nav-link').addEventListener('mouseover', mouseOver);
+// document.getElementsByClassName('nav-link').addEventListener('mouseout', mouseOut);
 
-function mouseOver() {
-	document.getElementsByClassName('nav-link').style.color = 'red';
-}
+// function mouseOver() {
+// 	document.getElementsByClassName('nav-link').style.color = 'red';
+// }
 
-function mouseOut() {
-	document.getElementsByClassName('nav-link').style.color = 'black';
-}
+// function mouseOut() {
+// 	document.getElementsByClassName('nav-link').style.color = 'black';
+// }
